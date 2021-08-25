c64e0f1e89022f5145be40641c46fb0405df4119
roninJosue <roninjosue88@gmail.com>
2021-08-25 12:40:52 -0600

    Adding label to checkbox.

--- src/components/Ingredient.jsx
+++ src/components/Ingredient.jsx
@@ -3,10 +3,12 @@ import React from "react";
 const Ingredient = (props) => {
   return (
     <li className='ingredients-item'>
-      <input type="checkbox"/>
-      <strong>{props.ingredient.quantity}</strong> &nbsp;
-      {props.ingredient.unit ? (`${props.ingredient.unit} de `) : ' '}
-      <strong>{props.ingredient.ingredient}</strong>
+      <label htmlFor={props.ingredient.id}>
+        <input id={props.ingredient.id} type="checkbox"/>
+        <strong>{props.ingredient.quantity}</strong> &nbsp;
+        {props.ingredient.unit ? (`${props.ingredient.unit} de `) : ' '}
+        <strong>{props.ingredient.ingredient}</strong>
+      </label>
     </li>
   )
 }
