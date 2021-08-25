375c75b45d5fd780ed8f8b61fd6b2d583b9e4df7
roninJosue <roninjosue88@gmail.com>
2021-08-25 13:03:08 -0600

    Adding width and height to image.

--- src/components/ImageRecipe.jsx
+++ src/components/ImageRecipe.jsx
@@ -5,7 +5,7 @@ import {Image} from "react-bootstrap";
 const ImageRecipe = () => {
   return (
     <section id="recipe-image">
-      <Image src={image} fluid rounded alt='Nacatamal'/>
+      <Image src={image} width='100%' height='100%' fluid rounded alt='Nacatamal'/>
     </section>
   );
 }
